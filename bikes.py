e891343c5cd0b8b042e5a97a6d5c6f4d0df5b79c
Corey <coreyengelhart@Coreys-MacBook-Air.local>
2019-06-20 07:30:32 -0500

    Add bikes.py to repo

---
+++ bikes.py
@@ -0,0 +1,10 @@
+class Bikes:
+    def __init__(self, type, color, wheel_size, brake_type, price_range):
+        self.type = type
+        self.color = color
+        self.wheel_size = wheel_size
+        self.brake_type = brake_type
+        self.price_range = price_range
+
+
+    
