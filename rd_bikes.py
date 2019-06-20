67d3f5a6f45604ba06c01ab1e653fa3e4d0643a0
Corey <coreyengelhart@Coreys-MacBook-Air.local>
2019-06-20 09:12:19 -0500

    Finish rd_bikes.py with fixed call for testing methods

--- rd_bikes.py
+++ rd_bikes.py
@@ -3,13 +3,19 @@ import math
 
 class RdBikes(Bikes):
 
-    def __init__(self, type, color, weight, wheel_size, brake_type, price, max_speed, frame_material, safey_score):
+    def __init__(self, type, color, weight, wheel_size, brake_type, price, max_speed, frame_material, safety_score):
         Bikes.__init__(self, type, color, weight, wheel_size, brake_type, price)
         self.max_speed = max_speed
         self.frame_material = frame_material
-        self.safey_score = safey_score
+        self.safety_score = safety_score
 
 
-    def danger_score(self, weight, brake_type, price, max_speed, safety_score):
-        danger_algo = ((.1 * weight) + (.1 * brake_type) + (.1 * price) + (.3 * max_speed) (.4 * safety_score))
-        print(danger_algo)
+    def danger_score(self):
+        danger_algo = ((.1 * self.weight) + (.4 * self.price) + (.5 * self.max_speed))
+        final_danger_score = danger_algo * self.safety_score
+        print(final_danger_score)
+
+
+new_bike = RdBikes('road', 'blue', 25, 26, 'disc', 2001, 42, 'fiberglass', 98)
+
+new_bike.danger_score()
